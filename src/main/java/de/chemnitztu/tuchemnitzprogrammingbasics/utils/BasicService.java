606fde3384b328a9d4412a50915397a63426b8fb
Shamin Asfaq <shamin.asfaq@gmail.com>
2021-10-15 18:37:13 +0600

    Proper Tests Added.

--- src/main/java/de/chemnitztu/tuchemnitzprogrammingbasics/utils/BasicService.java
+++ src/main/java/de/chemnitztu/tuchemnitzprogrammingbasics/utils/BasicService.java
@@ -6,7 +6,24 @@ import java.util.Arrays;
 
 @Service
 public class BasicService {
-    public void findGCD(long a, long b) {
+    Boolean[] centralPrimeFlags;
+
+    /**
+     * This is the constructor
+     */
+    public BasicService() {
+        this.centralPrimeFlags = makePrime();
+    }
+
+    //  1
+    //  Start
+    /**
+     * GCD Calculation following the Euclidean Method
+     * @param a is a number
+     * @param b is a number
+     * @return long
+     */
+    public long findGCD(long a, long b) {
         long maxx = Math.max(a, b);
         long minn = Math.min(a, b);
         long modd;
@@ -16,9 +33,17 @@ public class BasicService {
             maxx = minn;
             minn = modd;
         } while (modd != 0);
-        System.out.println("GCD(" + a + "," + b + ") = " + maxx);
+        return maxx;
     }
+    //  Finished
 
+    //  2
+    //  Start
+    /**
+     * Prime number generation following the Sieve of Eratosthenes
+     * @param flag is a Boolean Array
+     * @param limit is the inclusive range to make primes within
+     */
     public void sieve(Boolean[] flag, int limit) {
         for (int current = 2; current <= limit; current++) {
             if (flag[current]) {
@@ -28,19 +53,99 @@ public class BasicService {
             }
         }
     }
+    //  Finished
 
+    //  3
+    //  Start
+    /**
+     * Calling the SIEVE method from this, hence the name.
+     * @return Boolean[]
+     */
     public Boolean[] makePrime() {
-        int size = 1000;
-        Boolean[] flag = new Boolean[size+100];
+        int size = 10000000;
+        Boolean[] flag = new Boolean[size + 100];
         Arrays.fill(flag, Boolean.TRUE);
-
         sieve(flag, size);
-
         return flag;
     }
+    //  Finished
 
+    //  4
+    //  Start
+    /**
+     *
+     * @param number is a parameter which we are tesing if it is a Prime number or not.
+     * @return Boolean
+     */
     public Boolean isPrime(int number) {
-        Boolean[] primeFlags = makePrime();
-        return primeFlags[number];
+        return centralPrimeFlags[number];
     }
+    //  Finish
+
+    //  5
+    //  Start
+    /**
+     * Addition of Two absurdly large numbers
+     * @param firstNumber is a String
+     * @param secondNumber is a String
+     */
+    public String addTwoHugeNumbers(String firstNumber, String secondNumber) {
+        int firstNumberLength = firstNumber.length();
+        int secondNumberLength = secondNumber.length();
+
+        int minn = Math.min(firstNumberLength, secondNumberLength);
+        int maxx = Math.max(firstNumberLength, secondNumberLength);
+
+        int lengthDifference = maxx - minn;
+
+        if (maxx == firstNumberLength) {
+            StringBuilder secondNumberBuilder = new StringBuilder(secondNumber);
+            secondNumberBuilder = new StringBuilder(new StringBuffer(secondNumberBuilder).reverse().toString());
+            for (int inc = 0; inc < lengthDifference; inc++) {
+                secondNumberBuilder.append('0');
+            }
+            secondNumber = new StringBuffer(secondNumberBuilder).reverse().toString();
+        } else {
+            StringBuilder firstNumberBuilder = new StringBuilder(firstNumber);
+            firstNumberBuilder = new StringBuilder(new StringBuffer(firstNumberBuilder).reverse().toString());
+            for (int inc = 0; inc < lengthDifference; inc++) {
+                firstNumberBuilder.append('0');
+            }
+            firstNumber = new StringBuffer(firstNumberBuilder).reverse().toString();
+        }
+
+        StringBuilder sum = new StringBuilder();
+        int carry = 0;
+        for (int dec = maxx - 1; dec >=0; dec--) {
+            int up = Integer.parseInt(String.valueOf(firstNumber.charAt(dec)));
+            int down = Integer.parseInt(String.valueOf(secondNumber.charAt(dec)));
+
+            up += down + carry;
+            sum.append(up % 10);
+            carry = up/10;
+        }
+
+        if(carry > 0) {
+            sum.append(carry);
+        }
+        sum = new StringBuilder(new StringBuffer(sum.toString()).reverse().toString());
+        return sum.toString();
+    }
+    //  Finished
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/test/java/de/chemnitztu/tuchemnitzprogrammingbasics/utils/BasicServiceTest.java
+++ src/test/java/de/chemnitztu/tuchemnitzprogrammingbasics/utils/BasicServiceTest.java
@@ -1,31 +1,59 @@
 package de.chemnitztu.tuchemnitzprogrammingbasics.utils;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-@RunWith(SpringRunner.class)
 public class BasicServiceTest {
-    @Autowired BasicService basicService;
+    @Autowired
+    BasicService basicService;
 
     @Test
-    public void findGCD() {
-        basicService.findGCD(10, 45);
-        basicService.findGCD(1701, 3768);
+    @DisplayName("GCD")
+    public void testFindGCD() {
+        assertAll(
+                () -> assertEquals(5, basicService.findGCD(10, 45)),
+                () -> assertEquals(3, basicService.findGCD(1701, 3768)),
+                () -> assertEquals(17, basicService.findGCD(17, 34))
+        );
     }
 
     @Test
-    public void makePrime() {
-        basicService.isPrime(2);
-        basicService.isPrime(3);
-        basicService.isPrime(4);
-        basicService.isPrime(5);
-        basicService.isPrime(6);
-        basicService.isPrime(7);
+    @DisplayName("Prime Checker")
+    public void testIsPrime() {
+        assertAll(
+                () -> assertEquals(true, basicService.isPrime(2)),
+                () -> assertEquals(true, basicService.isPrime(3)),
+                () -> assertEquals(false, basicService.isPrime(4)),
+                () -> assertEquals(true, basicService.isPrime(5)),
+                () -> assertEquals(false, basicService.isPrime(6)),
+                () -> assertEquals(true, basicService.isPrime(7)),
+                () -> assertEquals(false, basicService.isPrime(8)),
+                () -> assertEquals(false, basicService.isPrime(9)),
+                () -> assertEquals(false, basicService.isPrime(10)),
+                () -> assertEquals(true, basicService.isPrime(11)),
+                () -> assertEquals(true, basicService.isPrime(991))
+        );
+    }
+
+    @Test
+    @DisplayName("ADD")
+    public void testAddTwoHugeNumbers() {
+        assertAll(
+                () -> assertEquals("15", basicService.addTwoHugeNumbers("5", "10")),
+                () -> assertEquals("374", basicService.addTwoHugeNumbers("53", "321")),
+                () -> assertEquals("138", basicService.addTwoHugeNumbers("69", "69")),
+                () -> assertEquals("1000", basicService.addTwoHugeNumbers("999", "1")),
+                () -> assertEquals("1998", basicService.addTwoHugeNumbers("999", "999")),
+                () -> assertEquals("2", basicService.addTwoHugeNumbers("1", "1")),
+                () -> assertEquals("88877445123412", basicService.addTwoHugeNumbers("854", "88877445122558")),
+                () -> assertEquals("1111111110", basicService.addTwoHugeNumbers("123456789", "987654321")),
+                () -> assertEquals("0", basicService.addTwoHugeNumbers("0", "0"))
+        );
     }
 }
+
